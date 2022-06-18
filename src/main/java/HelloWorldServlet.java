a9892fe01263ac00c94ee42033198a947fb12fd6
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-18 14:24:53 -0500

    Hello World servlet exercise complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,7 +10,8 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
