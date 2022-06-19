707ab0605c8be42c4d9f2995cdb735d9430a2ff6
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-19 13:28:31 -0500

    javaBeans exercise in progress

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,7 @@
+import model.Album;
+
+public class BeanTest {
+    public static void main(String[] args) {
+        Album OliviaRodrigo = new Album(1, "Olivia Rodrigo", "Good 4 U", 2019, )
+    }
+}
